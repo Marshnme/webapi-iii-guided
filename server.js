a2d414e906f1a19116e85e3624d8500fae18d43d
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-10-30 13:30:08 -0400

    gatekeeper

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require("helmet")
 const hubsRouter = require('./hubs/hubs-router.js');
-
+const morgan = require("morgan")
 const server = express();
 
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(new Date().toISOString(), `${req.method} ${req.originalUrl}`)
+  console.log(`[${new Date().toISOString()}]`, `${req.method} ${req.originalUrl}`)
 
   next();
 };
 
+
+function gateKeeper(req,res,next){
+  const password = req.headers.password || "";
+
+  if(password.toLowerCase() === "mellon"){
+    next();
+  }else if(!password){
+    res.status(400).json({message:"Must include pass"})
+  }else{
+    res.status(401).json({message:"YOU CAN NOT PASS"})
+  }
+}
+
 //global middleware
 server.use(helmet());
 server.use(express.json());
-server.use(logger)
+server.use(gateKeeper);
+server.use(logger);
+server.use(morgan("dev"));
 
 server.use('/api/hubs', hubsRouter);
 
