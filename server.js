4183078fb795aabd6ff4b5aeb72015495e090a8c
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-10-30 13:00:56 -0400

    logger functional

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require("helmet")
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+//custom middleware
+
+function logger(req, res, next) {
+  console.log(new Date().toISOString(), `${req.method} ${req.originalUrl}`)
+
+  next();
+};
+
+//global middleware
+server.use(helmet());
 server.use(express.json());
+server.use(logger)
 
 server.use('/api/hubs', hubsRouter);
 
